17d31b6531dbfb6c7029c596d8610034e43b925d
Rylie Nelson <ryliemn@gmail.com>
2015-11-22 20:21:53 -0800

    Started on front end, basic data retrieval laid out

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,23 @@
-console.log("hellO");
+var djeneratorApp = angular.module('djeneratorApp', []);
+
+djeneratorApp.config(['$interpolateProvider', function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[');
+  $interpolateProvider.endSymbol(']}');
+}]);
+
+djeneratorApp.controller('GeneratorCtrl', function ($scope, $http) {
+  $http({
+    method: 'GET',
+    url: '/random_name'
+  }).then(function successCallback(response) {
+    $scope.update_name(response.data['name']);
+  }, function errorCallback(response) {
+    console.log('something bad happened');
+  });
+
+
+
+  $scope.update_name = function(newName) {
+    $scope.random_name = newName;
+  }
+});
